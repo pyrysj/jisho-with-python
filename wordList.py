753b829eb4edb984112ed175f65bc188a19062aa
Pyry P <89637387+pyrysj@users.noreply.github.com>
2023-05-30 20:32:17 +0300

    begin splitting into modules, basic struct

---
+++ wordList.py
@@ -0,0 +1,18 @@
+# outline for module
+
+# first, read the file
+# second, pick out the words from the file
+# third, merge the words and provide the tags for if the word is a noun etc.
+# fourth, compile all information in the third step, save to intermediate file. 
+
+def readFile():
+    pass 
+
+def pickWords():
+    pass
+
+def mergeTerms():
+    pass
+
+def generateFile():
+    pass 
